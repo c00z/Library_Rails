041959aac52864e6b2ab4dfcb756a28f79f2036f
c00z <zcusimano@gmail.com>
2017-01-27 15:06:44 -0800

    bcrypt running hard n fast

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.create(user_params)
+    redirect_to root_path
   end
 
 
@@ -27,6 +28,12 @@ class UsersController < ApplicationController
 
   def destroy
     # stretch
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users
 
   get '/users/new' => 'users#new'
+  post '/users' =>  'users#create'
 
 end
