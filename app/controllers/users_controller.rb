62c25b483e6c34a2429b5f78e19a4d711d5e1fa1
c00z <zcusimano@gmail.com>
2017-01-27 15:13:16 -0800

    users by id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
 
   def show
-
+    @user = User.find_by_id(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   get '/users/new' => 'users#new'
   post '/users' =>  'users#create'
+  get '/users/:id' => 'users#show'
 
 end
