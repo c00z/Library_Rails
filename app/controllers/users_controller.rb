d116f8b81c88af1f6c753114b93181c343352c9b
c00z <zcusimano@gmail.com>
2017-01-27 14:36:29 -0800

    Initial setup done. First model and view setup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+
+  def show
+
+  end
+
+  def edit
+    # stretch
+  end
+
+  def update
+    # stretch
+  end
+
+  def destroy
+    # stretch
+  end
+
+
+end
