6a8f2c56fb2196caef4d3f9b55cbb439804de22c
c00z <zcusimano@gmail.com>
2017-01-28 11:25:43 -0800

    library_users controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,16 @@ Rails.application.routes.draw do
   get '/users/new' => 'users#new'
   post '/users' =>  'users#create'
   get '/users/:id' => 'users#show'
+
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
   get '/libraries' => 'libraries#index'
   get '/libraries/new' => 'libraries#new', as: 'new_library'
   post '/libraries' => 'libraries#create'
   get '/libraries/:id' => 'libraries#show'
 
-  
+  get '/users/:user_id/libraries' => 'library_users#index', as: 'user_libraries'
+
 end
